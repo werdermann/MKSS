24fc5397758c52db5a766c38552242494411e1c8
Lukas <39371330+werdermann@users.noreply.github.com>
2021-04-14 13:04:44 +0200

    Finished task 1.1

--- Aufgabe1/src/Bestellliste.java
+++ Aufgabe1/src/Bestellliste.java
@@ -1,94 +1,80 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
-public class Bestellliste 
-{
-	private Produkt[] produkte = new Produkt[5];
-	int index = 0;
+public class Bestellliste {
+	private ArrayList<Produkt> produkte = new ArrayList<>();
 
 	public void menuloop() 
 	{
-		int eingabe ;
-		do 
-		{
+		int eingabe;
+		do {
 			menueAusgeben();
 			eingabe = Eingabe.leseInt();
-			switch ( eingabe ) 
+			switch(eingabe)
 			{
 				case 0: break ;
-				case 1: materialBestellen(index++); break ;
-				case 2: dienstleistungBestellen(index++); break ;
+				case 1: materialBestellen(); break ;
+				case 2: dienstleistungBestellen(); break ;
 				default: System.out.println("ungültig" ); break ;
 			}
-		} 
-		while( eingabe != 0 && index < produkte.length);
+		} while(eingabe != 0);
 		bestellungSortieren() ;
 		bestellungBeenden() ;
 	}
 	
-	private void menueAusgeben() 
-	{
+	private void menueAusgeben() {
 		System.out.println("Was wollen Sie machen?");
 		System.out.println("(0) Bestellung beenden");
 		System.out.println("(1) Material bestellen");
 		System.out.println("(2) Dienstleistung bestellen");
 	}
 	
-	private void bestellungSortieren() 
-	{
-		for(int i = 0; i<produkte.length-1; i++) 
-		{
-			for(int j = 0; j<produkte.length-1; j++) 
-			{
-				if( produkte[j+1] != null &&
-					produkte[j+1].gibPreis ()<produkte[j].gibPreis()) 
-				{
-					Produkt temp = produkte[j + 1] ;
-					produkte[j+1] = produkte[ j ] ;
-					produkte[j] = temp ;
+	private void bestellungSortieren() {
+		for(int i = 0; i<produkte.size()-1; i++) {
+			for(int j = 0; j<produkte.size()-1; j++) {
+				if( produkte.get(j + 1) != null &&
+					produkte.get(j + 1).gibPreis ()< produkte.get(j).gibPreis()) {
+					Produkt temp = produkte.get(j + 1);
+					produkte.set(j + 1, produkte.get(j));
+					produkte.set(j, temp);
 				}
 			}
 		}
 	}
 	
-	private void materialBestellen(int index) 
-	{
+	private void materialBestellen() {
 		System.out.println("Name ");
 		String l = Eingabe.leseString();
 		System.out.println("Stückpreis ");
 		int p = Eingabe.leseInt();
 		System.out.println("Anzahl ");
 		int s = Eingabe.leseInt();
-		produkte[index] = new Material(l, p, s) ;
+		produkte.add(new Material(l, p, s));
 	}
 	
-	private void dienstleistungBestellen(int index) 
-	{
+	private void dienstleistungBestellen() {
 		System.out.println("Leisungsart ");
 		String l = Eingabe.leseString();
 		System.out.println("Personenzahl ");
 		int p = Eingabe.leseInt();
 		System.out.println("Stunden ");
 		int s = Eingabe.leseInt();
-		produkte[index] = new Dienstleistung(l, p, s) ;
+		produkte.add(new Dienstleistung(l, p, s));
 	}
 	
-	private void bestellungBeenden() 
-	{
+	private void bestellungBeenden() {
 		int sum = 0;
-		for(int i = 0; i < produkte.length; i++) 
-		{
-			if(produkte[i] != null) 
-			{
-				System.out.print(produkte[i] + " ");
-				System.out.println(preisAufbereiten(produkte[i].gibPreis()));
-				sum += produkte[i].gibPreis();
+		for (Produkt produkt : produkte) {
+			if (produkt != null) {
+				System.out.print(produkt + " ");
+				System.out.println(preisAufbereiten(produkt.gibPreis()));
+				sum += produkt.gibPreis();
 			}
 		}
 		System.out.println("Gesamtsumme: "+preisAufbereiten(sum));
 	}
 
-	private String preisAufbereiten(int preisInCent)
-	{
+	private String preisAufbereiten(int preisInCent) {
 		return (preisInCent / 100) + "." + (preisInCent % 100 < 10 ? "0" : "")
 			+ preisInCent % 100 + " EUR";
 	}
