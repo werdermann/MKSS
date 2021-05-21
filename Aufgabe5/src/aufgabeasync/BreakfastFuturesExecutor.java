002823a034d0a3ac15b790bed5a0f4315ad203ee
Lukas <39371330+werdermann@users.noreply.github.com>
2021-05-21 14:58:31 +0200

    Updated BreakfastFutures

--- Aufgabe5/src/aufgabeasync/BreakfastFuturesExecutor.java
+++ Aufgabe5/src/aufgabeasync/BreakfastFuturesExecutor.java
@@ -1,12 +1,81 @@
 package aufgabeasync;
 
+import java.util.Collection;
+import java.util.List;
 import java.util.Timer;
 import java.util.TimerTask;
 import java.util.concurrent.*;
 
-public class BreakfastFutures {
+public class BreakfastFuturesExecutor {
 
     public static void main(String[] args) throws InterruptedException, ExecutionException {
+
+        ExecutorService server = new ExecutorService() {
+            @Override
+            public void shutdown() {
+
+            }
+
+            @Override
+            public List<Runnable> shutdownNow() {
+                return null;
+            }
+
+            @Override
+            public boolean isShutdown() {
+                return false;
+            }
+
+            @Override
+            public boolean isTerminated() {
+                return false;
+            }
+
+            @Override
+            public boolean awaitTermination(long timeout, TimeUnit unit) throws InterruptedException {
+                return false;
+            }
+
+            @Override
+            public <T> Future<T> submit(Callable<T> task) {
+                return null;
+            }
+
+            @Override
+            public <T> Future<T> submit(Runnable task, T result) {
+                return null;
+            }
+
+            @Override
+            public Future<?> submit(Runnable task) {
+                return null;
+            }
+
+            @Override
+            public <T> List<Future<T>> invokeAll(Collection<? extends Callable<T>> tasks) throws InterruptedException {
+                return null;
+            }
+
+            @Override
+            public <T> List<Future<T>> invokeAll(Collection<? extends Callable<T>> tasks, long timeout, TimeUnit unit) throws InterruptedException {
+                return null;
+            }
+
+            @Override
+            public <T> T invokeAny(Collection<? extends Callable<T>> tasks) throws InterruptedException, ExecutionException {
+                return null;
+            }
+
+            @Override
+            public <T> T invokeAny(Collection<? extends Callable<T>> tasks, long timeout, TimeUnit unit) throws InterruptedException, ExecutionException, TimeoutException {
+                return null;
+            }
+
+            @Override
+            public void execute(Runnable command) {
+
+            }
+        };
 
         long start = System.currentTimeMillis();
 
