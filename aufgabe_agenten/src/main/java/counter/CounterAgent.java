228ace16cbfefa740bafe8c0e7a8a6e70cb82917
Lukas <39371330+werdermann@users.noreply.github.com>
2021-06-16 16:01:38 +0200

    Finished Task 9.2

--- aufgabe_agenten/src/main/java/counter/CounterAgent.java
+++ aufgabe_agenten/src/main/java/counter/CounterAgent.java
@@ -1,7 +1,12 @@
 package counter;
 
+import helloworld.HelloWorldAgent;
+import jadex.base.PlatformConfigurationHandler;
+import jadex.base.Starter;
+import jadex.bridge.IExternalAccess;
 import jadex.bridge.IInternalAccess;
 import jadex.bridge.service.annotation.OnStart;
+import jadex.bridge.service.types.cms.CreationInfo;
 import jadex.micro.annotation.Agent;
 
 @Agent
@@ -9,10 +14,20 @@ public class CounterAgent
 {
 	@Agent
 	protected IInternalAccess agent;
-	
+
 	@OnStart
-	public void body()
-	{
+	public void body() {
 		// count from 0 to 9 and wait one second after each print out
+		for(int i = 0; i < 10; i++) {
+			agent.waitForDelay(1000).get();
+			System.out.println("Agent " + agent.getId() + " " + i);
+		}
+
+		agent.killComponent();
+	}
+
+	public static void main(String[] args) {
+		IExternalAccess platform = Starter.createPlatform(PlatformConfigurationHandler.getDefaultNoGui()).get();
+		platform.createComponent(new CreationInfo().setFilenameClass(CounterAgent.class)).get();
 	}
 }
